004a02ac969d3d5f0a5b17996c683eb692d9fada
Tim Dussinger <tritowntim@gmail.com>
2012-07-26 17:02:58 -0400

    Code updated thru "Responding request handlers with non-blocking operations"

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,21 +2,25 @@
 var exec = require('child_process').exec
 
 function start(res) {
-	log('Request handler "start" was called')
+	log('START begin')
 
-	exec('ls -lah', function(error, stdout, stderr) {
-		console.log(stdout)
-		res.writeHead(200, {'Content-Type' : 'text/plain'})
-		res.write(stdout)
-		res.end()
-	})
+	exec('find /',
+		{timeout: 20000, maxBuffer: 20000 * 1024},
+		function(error, stdout, stderr) {
+			res.writeHead(200, {'Content-Type' : 'text/plain'})
+			res.write(stdout)
+			res.end()
+			log('START end')
+		})
 
 }
 
 function upload(res) {
+		log('UPLOAD begin')
 		res.writeHead(200, {'Content-Type' : 'text/plain'})
 		res.write('Request handler "upload" was called')
 		res.end()
+		log('UPLOAD end')
 }
 
 function log(txt) {
