5ab4374e8197188797de3a4ebda489f266fd0fc0
Tim Dussinger <tritowntim@gmail.com>
2012-07-26 16:19:46 -0400

    Changes for blocking in "Blocking and non-blocking"

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,24 @@
 
 function start() {
-	console.log('Request handler "start" was called.')
+	log('Request handler "start" was called, sleep ten seconds...')
+
+	function sleep(millisecs) {
+		var start = new Date().getTime()
+		while (new Date().getTime() < start + millisecs);
+	}
+
+	sleep(10000)
+	log('Sleep has ended')
 	return 'Hello Start'
 }
 
 function upload() {
-	console.log('Request handler "upload" was called.')
+	log('Request handler "upload" was called.')
 	return 'Hello Upload'
+}
+
+function log(txt) {
+	console.log(new Date() + ' ' + txt)
 }
 
 exports.start = start
